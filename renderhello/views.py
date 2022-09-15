0b1933289a680dbd75a38f4597a08c063bd4e60a
felixd3v <fmhcde@gmail.com>
2022-09-15 17:15:54 -0500

    configure urls

--- renderhello/views.py
+++ renderhello/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse #import
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Good morning, good afternoon, good evening. Nice to meet you!")
+
